35e4e01a3ba52adda4b36e09f8db9983db669579
AVK9 <64771146+AVK9@users.noreply.github.com>
2023-11-03 13:01:44 +0000

    Deploying to gh-pages from @ AVK9/team-proj-js-00@e63b14bdc059e870687030b1fa16da737eff16ac 🚀

--- assets/index-032c61a8.js
+++
@@ -1,2 +0,0 @@
-import"./styles-96598d67.js";import"./gallery-pixabay-a414beb4.js";import"./serviceMovie-794b6d6c.js";console.log("sdfsdfsdfsdfsdf");
-//# sourceMappingURL=index-032c61a8.js.map
---
+++ assets/index-b76f4ba6.js
@@ -0,0 +1,2 @@
+import"./styles-987f348b.js";console.log("sdfsdfsdfsdfsdf");
+//# sourceMappingURL=index-b76f4ba6.js.map
